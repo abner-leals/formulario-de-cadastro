f52aea35aa8e3b1c4c74a400f4298ef34921a312
Abner Leal <abner.leal.silva@gmail.com>
2022-06-18 11:03:37 -0300

    formatacao do form ok

--- formulario/src/Pages/Cadastro/index.jsx
+++ formulario/src/Pages/Cadastro/index.jsx
@@ -3,18 +3,58 @@ import * as M from "@mui/material";
 import styled from "styled-components";
 export default function Form() {
   return (
-    <FormStyled>
-      <M.TextField required id="outlined-required" label="Nome" />
-      <M.TextField required id="outlined-required" label="Idade" />
-      <M.TextField required id="outlined-required" label="Endereco" />
-      <M.TextField required id="outlined-required" label="Endereco" />
-      <M.TextField required id="outlined-required" label="Endereco" />
-      <M.TextField required id="outlined-required" label="Endereco" />
-    </FormStyled>
+    <>
+      <M.Box
+        width={"25ch"}
+        component={FormEstilizado}
+        sx={{
+          "& .MuiTextField-root": { m: 2, width: "100%" },
+        }}
+        onSubmit={() => console.log("ok")}
+      >
+        <M.TextField
+          error
+          id="outlined-error"
+          label="Error"
+          defaultValue="Hello World"
+        />
+        <M.TextField
+          error
+          id="outlined-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+
+        <M.TextField
+          error
+          id="filled-error"
+          label="Error"
+          defaultValue="Hello World"
+        />
+        <M.TextField
+          error
+          id="filled-error-helper-text"
+          label="Error"
+          defaultValue="Hello World"
+          helperText="Incorrect entry."
+        />
+
+        <M.Button variant="outlined" type="submit" size="large">
+          Cadastrar
+        </M.Button>
+      </M.Box>
+    </>
   );
 }
-
-const FormStyled = styled.form`
+const FormEstilizado = styled.form`
   display: flex;
+  align-items: center;
   flex-direction: column;
+  box-shadow: inset 0 0 0em, 0 0 1em grey;
+  padding: 50px;
+  border-radius: 5%;
+  input {
+    color: #ed9;
+  }
 `;
